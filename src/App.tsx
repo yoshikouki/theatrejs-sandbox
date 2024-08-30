51f4dcc3c6e2beb7bfcaf15b084d1e6f0f379659
yoshikouki <yoshikouki@pepabo.com>
2024-08-30 14:26:49 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import { Canvas } from "@react-three/fiber";
 import { getProject } from "@theatre/core";
-import { SheetProvider } from "@theatre/r3f";
+import { SheetProvider, editable as e } from "@theatre/r3f";
+import { PerspectiveCamera } from "@theatre/r3f";
 import extension from "@theatre/r3f/dist/extension";
 import studio from "@theatre/studio";
 
 studio.initialize();
 studio.extend(extension);
 
-// our Theatre.js project sheet, we'll use this later
-const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
-
 const App = () => {
   return (
     <main className="h-full w-full">
-      <Canvas
-        camera={{
-          position: [5, 5, -5],
-          fov: 75,
-        }}
-      >
-        <SheetProvider sheet={demoSheet}>
+      <Canvas gl={{ preserveDrawingBuffer: true }}>
+        <SheetProvider sheet={getProject("Demo Project").sheet("Demo Sheet")}>
+          <PerspectiveCamera
+            theatreKey="Camera"
+            makeDefault
+            position={[5, 5, 5]}
+            fov={75}
+          />
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <mesh>
+          <e.mesh theatreKey="mesh">
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial color="orange" />
-          </mesh>
+          </e.mesh>
         </SheetProvider>
       </Canvas>
     </main>
